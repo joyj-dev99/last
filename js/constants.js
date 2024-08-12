788effd61ea4f7007860d0fccdd3d1d02eee52fb
joyj-dev99 <joyj.dev99@gmail.com>
2024-08-12 12:46:52 +0900

    몬스터 공격 객체에 대한 상수 추가

--- js/constants.js
+++ js/constants.js
@@ -3,3 +3,4 @@ export const MONSTER_CATEGORY = 0x0002;
 export const TILE_CATEGORY = 0x0004;
 export const OBJECT_CATEGORY = 0x0005;
 export const PLAYER_ATTACK_CATEGORY = 0x0006;
+export const MONSTER_ATTACK_CATEGORY = 0x0007;
