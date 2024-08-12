09f9801d53bfe83c5537b16d2bc6413e1ca7bed2
joyj-dev99 <joyj.dev99@gmail.com>
2024-08-12 18:08:42 +0900

    update에서 slash 동기화시 x 좌표 + offsetX

--- js/Player.js
+++ js/Player.js
@@ -172,8 +172,9 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
             this.handleArrowKeyInput(playerVelocity, speed);
             
             if(this.isSwinging){ // 검을 휘두르는 동안에
+                const offsetX = this.isLookingRight ? 10 : -10; // 플레이어 방향에 따른 오프셋 설정
                 // 플레이어 위치에 slash 객체 동기화
-                this.slash.setPosition(this.x, this.y);
+                this.slash.setPosition(this.x + offsetX, this.y);
             }
         
             // Shift 키를 눌렀을 때 구르기 시작
