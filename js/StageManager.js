331dd6ce5614e3422abf8875070a2eea3a7a996b
정지은 <kindjjee@gmail.com>
2024-08-15 17:59:05 +0900

    조작키 변수 수정

--- js/StageManager.js
+++ js/StageManager.js
@@ -71,7 +71,7 @@ export default class StageManager {
                     tutorial.removeRightSign();
 
                     // 조작키 제거
-                    tutorial.removeKey();
+                    tutorial.endzKeyControlExplanation();
                 
                     // 코드의 위치 이동시키기
                     this.chord.setLocation(this.player.x, this.player.y - 50);
