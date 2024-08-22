f936df12caa34dc6015b1cd9bd19e8a50299ee38
joyj-dev99 <joyj.dev99@gmail.com>
2024-08-22 21:44:30 +0900

    충돌해결중

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -321,9 +321,49 @@ export function setupMap(scene, stageNumber, mapNumber) {
 
         //선물 맵의 아이템 위치
         if(name === 'item'){
-            Item.dropRandomReward(scene,scene.player,x,y,scene.dialog);
-        }
-    });
+
+            // 아이템 리스트 정의
+            const items = [
+                Item.MaxHeart_ITEM,
+                Item.PhantomCloak_ITEM,
+                Item.SwiftBoots_ITEM,
+                Item.UnknownAmulet_ITEM,
+                Item.QuantumHourglass_ITEM,
+                Item.HerbalMedicine_ITEM,
+                Item.PiratesSafe_ITEM,
+                Item.AncientPotion_ITEM,
+                Item.HeavyGloves_ITEM,
+                Item.arrow_10_ITEM
+            ];
+            // 랜덤으로 하나의 아이템 선택
+            const randomItem = items[Math.floor(Math.random() * items.length)];
+
+            // 선택된 아이템을 itemType으로 설정
+            scene.item = new Item({
+                scene: scene,
+                x: x,
+                y: y,
+                itemType: randomItem
+            });
+            // console.log("scene.player" + scene.player); 
+            // console.log("scene.item"+ scene.item);
+        
+
+            // 충돌 이벤트 설정
+            const unsubscribe = scene.matterCollision.addOnCollideStart({
+                objectA: scene.player,
+                objectB: scene.item,   
+                callback: eventData => { 
+                    const { bodyA, bodyB, gameObjectA, gameObjectB, pair } = eventData;
+                    scene.getItemSound.play();
+                    console.log("플레이어와 아이템 충돌");
+                    // 아이템 효과 적용하기 및 화면에 반영하기
+                    gameObjectB.applyItem(gameObjectA, scene.heartIndicator, scene.dialog);
+                    unsubscribe();
+                }
+            });
+    
+        }});
 
     // 몬스터 생성 - 플레이어가 생성된 이후에 생성되어야 함.
     objectLayer.objects.forEach(object => {
