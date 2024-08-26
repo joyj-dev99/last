7347e5da23d2969564ef73af3a8222f645877c0b
joyj-dev99 <joyj.dev99@gmail.com>
2024-08-26 14:48:34 +0900

    .

--- js/game.js
+++ js/game.js
@@ -26,7 +26,7 @@ const config = {
     physics: {
         default: 'matter',
         matter: {
-            debug: true ,
+            debug: false ,
             gravity: {y: 0},
         }
     },
